06c8d2a745549b33568422704375c174c2a0cbfe
wananyx <412828823@qq.com>
2019-02-12 21:25:07 +0800

    解决javax.persistence-api冲突问题

--- src/main/java/com/jst/basic/framework/BasicFrameworkApplication.java
+++ src/main/java/com/jst/basic/framework/BasicFrameworkApplication.java
@@ -2,8 +2,10 @@ package com.jst.basic.framework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.jst.basic.framework.dao")
 public class BasicFrameworkApplication {
 
     public static void main(String[] args) {
